01ff11a36ae8aa0f2dfb87e2a7f6c68ffa70085d
190103439 <190103439@stu.sdu.edu.kz>
2021-03-21 01:12:49 +0600

    post test

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -15,19 +15,19 @@ class PostTest extends TestCase
      */
     public function test_example()
     {
-        $response = $this->get('/');
+        $response = $this->get('/welcome');
 
         $response->assertStatus(200);
     }
-    public function test_post_id_get_request(){
+    public function test_post_id_get_request() {
         $response = $this->get('/post/1');
-        $response->assertStatus(500);
+        $response->assertStatus(200);
     }
-    public function test_get_unexisted_post(){
+    public function test_get_unexisted_post() {
         $response = $this->get('/post/10000');
-        $response->assertStatus(500);
+        $response->assertStatus(404);
     }
-    public function test_post_response(){
+    public function test_post_response() {
         $response = $this->get('/post/1');
         $response->assertViewHas('post');
     }
